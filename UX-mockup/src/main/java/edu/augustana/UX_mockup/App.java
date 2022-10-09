6665e5483c0899e0b84bd45500b8383f179f6de0
ilyasjamil <jamilon33@gmail.com>
2022-10-09 18:08:19 -0500

    fixed misaligned window

--- UX-mockup/src/main/java/edu/augustana/UX_mockup/App.java
+++ UX-mockup/src/main/java/edu/augustana/UX_mockup/App.java
@@ -19,7 +19,6 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
-        stage.setMaximized(true);
         stage.show();
     }
 
