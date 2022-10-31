efaef6c97b983369f419ab44d629785ae5a6c71d
Rijan <rijankafle20@augustana.edu>
2022-10-31 14:09:20 -0500

    Added an mose pressed event which makes a grey rectanngle when clicked in the canvas

--- ThreeDBoardCreator/src/main/java/ibis/boardcreator/ui/MainEditorController.java
+++ ThreeDBoardCreator/src/main/java/ibis/boardcreator/ui/MainEditorController.java
@@ -7,20 +7,21 @@ import ibis.boardcreator.datamodel.Tile;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.paint.Color;
 
 public class MainEditorController {
 
-	@FXML 
-	private Canvas canvasGrid;
-	@FXML
-    private ToggleButton addButton;
-
+	@FXML private Canvas canvasGrid;
+	@FXML private ToggleButton addButton;
+	@FXML private Slider elevationSlider;
+	
 	@FXML
 	private void initialize() {
-		GraphicsContext gc = canvasGrid.getGraphicsContext2D();
 		
+		GraphicsContext gc = canvasGrid.getGraphicsContext2D();
+	
 		//TODO: store the grid somewhere - we don't want to keep creating new Grid objects
 		Grid grid = new Grid();
 		
@@ -29,12 +30,21 @@ public class MainEditorController {
 		
 		for (int r = 0; r < grid.getNumRows(); r++) {
 			for (int c = 0; c < grid.getNumColumns(); c++) {
-				gc.setStroke(Color.GREY);
+				gc.setStroke(Color.BLACK);
 				double x = c * 50;
 				double y = r * 50;
 				gc.strokeRect(x, y, 50, 50);
 				Tile tile = grid.getTileAt(r, c);
+				
+				tile.setElevation(elevationSlider.getValue());
 				double elev = tile.getElevation();
+				
+				canvasGrid.setOnMousePressed(evt ->{
+					gc.setFill(Color.GREY);
+					gc.fillRect(evt.getX()-30, evt.getY()-15, 50, 50);
+				});
+				
+				
 				//TODO fillRect using an appropriate color, based on the elevation
 				
 			}
