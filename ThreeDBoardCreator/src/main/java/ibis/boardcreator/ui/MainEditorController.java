c4006d2f18cc52537fca09b6467d33082bd317ac
Jamil <ilyasjamil20@augustana.edu>
2022-11-01 10:55:23 -0500

    Fixed the color rgb combination

--- ThreeDBoardCreator/src/main/java/ibis/boardcreator/ui/MainEditorController.java
+++ ThreeDBoardCreator/src/main/java/ibis/boardcreator/ui/MainEditorController.java
@@ -62,7 +62,7 @@ public class MainEditorController {
 		int c = (int) (evt.getX() / TILE_SIZE);
 		int r = (int) (evt.getY() / TILE_SIZE);
 		Tile clickedTile = App.getGrid().getTileAt(r, c);
-		Color color = new Color( 0.52, 0.52, 0.52, elevationSlider.getValue()/10 );
+		Color color = new Color( 0, 0, 0, elevationSlider.getValue()/10 );
 		gc.setFill(color);
 		gc.fillRect(c * TILE_SIZE ,r * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 		clickedTile.setElevation(5);
